2313e108fb09604006bff708d0de144ab4abf700
fcivillini <fabio.civillini@objectway.it>
2025-03-31 11:58:57 +0200

    1.0.0: lunch with SpringBoot

---
+++ AwesomePizzaManagerBoot/src/main/java/com/fcivillini/awesomePizzaManagerBoot/AwesomePizzaManagerBootApplication.java
@@ -0,0 +1,14 @@
+package com.fcivillini.awesomePizzaManagerBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomePizzaManagerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomePizzaManagerBootApplication.class, args);
+    }
+
+}
+
